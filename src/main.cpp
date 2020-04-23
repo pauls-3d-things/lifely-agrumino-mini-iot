5749a083e424820734c214b43a3938f7b86a62ba
Paul Smith <code@uvwxy.de>
2020-04-23 12:52:45 +0200

    cleanup workspace

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <Arduino.h>
 
 #include "Agrumino.h"
+#include "config.h"
+#include "mini-iot-client.h"
 
 Agrumino agrumino;
+MiniIotClient miniIot("lifely01", MINI_IOT_SERVER, WIFI_SSID, WIFI_PASS);
 
 void setup() {
   Serial.begin(115200);
@@ -10,31 +13,25 @@ void setup() {
 }
 
 void loop() {
-  Serial.println("--------------------------------");
-  Serial.print("Temp: ");
-  Serial.print(agrumino.readTempC());
-  Serial.println("C");
+  float c = agrumino.readTempC();
+  float s = agrumino.readSoil();
+  float sr = agrumino.readSoilRaw();
+  float lx = agrumino.readLux();
+  float bl = agrumino.readBatteryLevel();
+  float bv = agrumino.readBatteryVoltage();
 
-  Serial.print("Soil: ");
-  Serial.print(agrumino.readSoil());
-  Serial.print("%, raw: ");
-  Serial.println(agrumino.readSoilRaw());
+  String payload =
+      String(c) + "," + String(s) + "," + String(sr) + "," + String(lx) + "," + String(bl) + "," + String(bv);
 
-  Serial.print("Lux: ");
-  Serial.print(agrumino.readLux());
-  Serial.println("lx");
-
-  Serial.print("Battery: ");
-  Serial.print(agrumino.readBatteryLevel());
-  Serial.print("%, ");
-  Serial.print(agrumino.readBatteryVoltage());
-  Serial.println("V");
+  miniIot.connectToWifi(); // this blocks until connected
+  miniIot.appendWithTimestamp("data.csv", payload);
+  Serial.println(payload);
 
   if (agrumino.isAttachedToUSB() && !agrumino.isLedOn()) {
     agrumino.turnLedOn();
-  } else if (agrumino.isLedOn()) {
+  } else if (!agrumino.isAttachedToUSB() && agrumino.isLedOn()) {
     agrumino.turnLedOff();
   }
 
-  delay(1000);
+  agrumino.deepSleepSec(3600);
 }
